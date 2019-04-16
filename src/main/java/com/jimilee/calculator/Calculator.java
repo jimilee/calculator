0aefcb0551962aad13c0598f3217b3ab42f0ae73
jimilee <1234567890jm48@gmail.com>
2019-04-16 14:36:10 +0900

    Add test &unit

--- src/main/java/com/jimilee/calculator/Calculator.java
+++ src/main/java/com/jimilee/calculator/Calculator.java
@@ -1,6 +1,7 @@
 package com.jimilee.calculator;
 
-import org.springframwork.stereotype.Service;
+import org.springframework.stereotype.Service;
+
 @Service
 public class Calculator{
   int sum(int a, int b){
