9e8271349d846aee0f97176a7d5889fae1693033
jimilee <1234567890jm48@gmail.com>
2019-05-02 13:52:24 +0900

    add acceptance_test.sh

---
+++ acceptance_test.sh
@@ -0,0 +1,7 @@
+#
+
+#!/bin/bash
+test --silent $(curl localhost:8090/sum?a=1\&b=2) -eq 3 || test -silent $(curl $(docker network inspect -f '{{range.IPAM.Config}}{{.Gateway}}{{end}}'bridge):8090/sum?a=1\&b=2) -eq 3
+
+
+
