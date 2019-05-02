897bc02c55091017e12ea762fd1b1ef032729238
jimilee <1234567890jm48@gmail.com>
2019-05-02 14:52:36 +0900

    add_acc3

--- acceptance_test.sh
+++ acceptance_test.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-test -silent $(curl localhost:5000/sum?a=1\&b=2) -eq 3 || \
-test -silent $(curl $(docker network inspect -f '{{range.IPAM.Config}}{{.Gateway}}{{end}}' bridge):5000/sum?a=1\&b=2) -eq 3
+test --silent $(curl localhost:8090/sum?a=1\&b=2) -eq 3 || \
+test --silent $(curl $(docker network inspect -f '{{range.IPAM.Config}}{{.Gateway}}{{end}}' bridge):8090/sum?a=1\&b=2) -eq 3
 
 
